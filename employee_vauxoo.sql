c6334188e93d873f65e40be559ca25a60ca1881e
Jessi-M <Jhessicam02@gmail.com>
2017-03-28 10:59:58 -0400

    Add files via upload

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,29 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+id integer PRIMARY KEY,
+first_name varchar (60),
+last_name varchar (60)
 );
 
 CREATE TABLE employee_department (
+id integer PRIMARY KEY,
+name varchar (60),
+description varchar (60)
 );
 
-CREATE TABLE employee_hobby (
-);
+INSERT INTO employee_department (id,name,description) VALUES
+(1,'Direccion General','Direccion General'),
+(2,'Direccion de operaciones','Direccion de operaciones de planta'),
+(3,'Administracion','Administracion'),
+(4,'Ventas','Ventas'),
+(5,'Compras','Compras'),
+(6,'Recursos humanos','recursos humanos');
 
--- ...
+ALTER TABLE employee ADD COLUMN department_id integer REFERENCES employee_department (id);
+
+INSERT INTO employee (id,first_name,last_name,department_id) VALUES
+(1,'Jhesica','Martinez',1),
+(2,'Hernan','Hernandez',2),
+(3,'Julio','Marana',3),
+(4,'Jerson','Valencia',1);
