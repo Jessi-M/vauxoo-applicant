b4c1e79ea66803100e3b61ec869bdc7459b294ff
Jessi-M <Jhessicam02@gmail.com>
2017-03-28 12:52:56 -0400

    Add files via upload
    cambios del scrip para cumplir los requerimientos de sql3

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,35 +3,10 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee_hobby (
-id integer PRIMARY KEY,
-name varchar (60),
-description varchar (60)
-);
-
-INSERT INTO employee_hobby (id,name,description) VALUES
-(1,'Videojuegos','jugar videojuegos'),
-(2,'Teatro','participar en obras'),
-(3,'Deportes','practicar deporte');
+ALTER TABLE employee ADD COLUMN boss_id integer REFERENCES employee (id);
 
 
-CREATE TABLE employees_hobbies (
-employee_id integer REFERENCES employee (id),
-hobby_id integer REFERENCES employee_hobby (id),
-PRIMARY KEY (employee_id, hobby_id)
-);
-
-INSERT INTO employees_hobbies (employee_id,hobby_id) VALUES
-(1,2),
-(1,3),
-
-(2,2),
-(2,3),
-
-(3,1),
-(3,2),
-
-(4,2),
-(4,3);
-
-
+UPDATE employee SET boss_id = 1 WHERE id = 1;
+UPDATE employee SET boss_id = 1 WHERE id = 2;
+UPDATE employee SET boss_id = 2 WHERE id = 3;
+UPDATE employee SET boss_id = 3 WHERE id = 4;
